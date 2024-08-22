cbcdab3c400072df4c2d05a83144509bf14e1f3f
Ana Mayordomo <anamayordomo@Anas-Laptop.localdomain>
2024-08-22 14:11:10 -0500

    Add email field to signup page

--- App_Login/forms.py
+++ App_Login/forms.py
@@ -1,14 +1,20 @@
+from django import forms 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta(UserCreationForm.Meta):
-        fields = ('username', 'password1', 'password2')  # Only include these fields
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')  # Only include these fields
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # Remove help text from fields
         self.fields['username'].help_text = None
+        self.fields['email'].help_text = None
         self.fields['password1'].help_text = None
         self.fields['password2'].help_text = None
 
