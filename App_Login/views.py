c512db380c87bd2444dcc3c1de17bdc08e411fd7
Ana Mayordomo <anamayordomo@Anas-Laptop.localdomain>
2024-08-22 13:31:45 -0500

    Login page

--- App_Login/views.py
+++ App_Login/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from . forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def signup(request):
@@ -14,3 +18,21 @@ def signup(request):
 
     context = {'form' : form, 'registered' : registered}
     return render(request, 'App_Login/signup.html', context)
+
+
+def login(request):
+    form = AuthenticationForm()
+    context = {'form' : form}
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if username is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+    return render(request, 'App_Login/login.html', context)
+
